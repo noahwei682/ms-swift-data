d8e407e40ffc7fab0fc883cea967b50d6660ac30
Noah682 <noahwei682@gmail.com>
2025-05-06 19:31:41 +0800

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -32,3 +32,55 @@ huggingface-cli upload amazon-grpo meta_Pet_Supplies_dict_title_grpo.json --repo
 huggingface-cli upload amazon-grpo merged_data.json --repo-type dataset
 
 
+
+
+huggingface-cli upload amazon-qwen-file merged_data.json --repo-type dataset
+huggingface-cli upload amazon-qwen-file qwen_meta_All_Beauty_details_sftdata_att_train_1800.json --repo-type dataset
+huggingface-cli upload amazon-qwen-file qwen_meta_All_Beauty_feature_sftdata_att_train_1800.json --repo-type dataset
+huggingface-cli upload amazon-qwen-file qwen_meta_All_Beauty_main_category_sftdata_att_train_1800.json --repo-type dataset
+huggingface-cli upload amazon-qwen-file qwen_meta_All_Beauty_price_sftdata_att_train_1800.json --repo-type dataset
+huggingface-cli upload amazon-qwen-file qwen_meta_All_Beauty_price_sftdata_att_train_1800.json --repo-type dataset
+huggingface-cli upload amazon-qwen-file qwen_meta_All_Beauty_Store_sftdata_att_train_1800.json --repo-type dataset
+huggingface-cli upload amazon-qwen-file qwen_meta_All_Beauty_title_sftdata_att_train_1800.json --repo-type dataset
+huggingface-cli upload amazon-qwen-file qwen_meta_Amazon_Fashion_description_sftdata_att_train_180.json --repo-type dataset
+huggingface-cli upload amazon-qwen-file qwen_meta_Amazon_Fashion_details_sftdata_att_train_1800.json --repo-type dataset
+huggingface-cli upload amazon-qwen-file qwen_meta_Amazon_Fashion_feature_sftdata_att_train_180.json --repo-type dataset
+huggingface-cli upload amazon-qwen-file qwen_meta_Amazon_Fashion_main_category_sftdata_att_train_180.json --repo-type dataset
+huggingface-cli upload amazon-qwen-file qwen_meta_Amazon_Fashion_price_sftdata_att_train_0.json --repo-type dataset
+huggingface-cli upload amazon-qwen-file qwen_meta_Amazon_Fashion_Store_sftdata_att_train_180.json --repo-type dataset
+huggingface-cli upload amazon-qwen-file qwen_meta_Amazon_Fashion_title_sftdata_att_train_180.json --repo-type dataset
+huggingface-cli upload amazon-qwen-file qwen_meta_Appliances_description_sftdata_att_train_180.json --repo-type dataset
+huggingface-cli upload amazon-qwen-file qwen_meta_Appliances_details_sftdata_att_train_1800.json --repo-type dataset
+huggingface-cli upload amazon-qwen-file qwen_meta_Appliances_feature_sftdata_att_train_180.json --repo-type dataset
+huggingface-cli upload amazon-qwen-file qwen_meta_Appliances_main_category_sftdata_att_train_180.json --repo-type dataset
+huggingface-cli upload amazon-qwen-file qwen_meta_Appliances_price_sftdata_att_train_30.json --repo-type dataset
+huggingface-cli upload amazon-qwen-file qwen_meta_Appliances_Store_sftdata_att_train_180.json --repo-type dataset
+huggingface-cli upload amazon-qwen-file qwen_meta_Appliances_title_sftdata_att_train_1800.json --repo-type dataset
+huggingface-cli upload amazon-qwen-file qwen_meta_Arts_Crafts_and_Sewing_description_sftdata_att_train_1800.json --repo-type dataset
+huggingface-cli upload amazon-qwen-file qwen_meta_Arts_Crafts_and_Sewing_details_sftdata_att_train_1800.json --repo-type dataset
+huggingface-cli upload amazon-qwen-file qwen_meta_Arts_Crafts_and_Sewing_feature_sftdata_att_train_1800.json --repo-type dataset
+huggingface-cli upload amazon-qwen-file qwen_meta_Arts_Crafts_and_Sewing_main_category_sftdata_att_train_1800.json --repo-type dataset
+huggingface-cli upload amazon-qwen-file qwen_meta_Arts_Crafts_and_Sewing_price_sftdata_att_train_1800.json --repo-type dataset
+huggingface-cli upload amazon-qwen-file qwen_meta_Arts_Crafts_and_Sewing_Store_sftdata_att_train_1800.json --repo-type dataset
+huggingface-cli upload amazon-qwen-file qwen_meta_Arts_Crafts_and_Sewing_title_sftdata_att_train_180.json --repo-type dataset
+huggingface-cli upload amazon-qwen-file qwen_meta_Automotive_description_sftdata_att_train_180.json --repo-type dataset
+huggingface-cli upload amazon-qwen-file qwen_meta_Automotive_details_sftdata_att_train_180.json --repo-type dataset
+huggingface-cli upload amazon-qwen-file qwen_meta_Automotive_feature_sftdata_att_train_180.json --repo-type dataset
+huggingface-cli upload amazon-qwen-file qwen_meta_Automotive_main_category_sftdata_att_train_180.json --repo-type dataset
+huggingface-cli upload amazon-qwen-file qwen_meta_Automotive_price_sftdata_att_train_0.json --repo-type dataset
+huggingface-cli upload amazon-qwen-file qwen_meta_Automotive_Store_sftdata_att_train_180.json --repo-type dataset
+huggingface-cli upload amazon-qwen-file qwen_meta_Automotive_title_sftdata_att_train_180.json --repo-type dataset
+huggingface-cli upload amazon-qwen-file qwen_meta_Baby_Products_description_sftdata_att_train_180.json --repo-type dataset
+huggingface-cli upload amazon-qwen-file qwen_meta_Baby_Products_details_sftdata_att_train_1800.json --repo-type dataset
+huggingface-cli upload amazon-qwen-file qwen_meta_Baby_Products_feature_sftdata_att_train_180.json --repo-type dataset
+huggingface-cli upload amazon-qwen-file qwen_meta_Baby_Products_main_category_sftdata_att_train_180.json --repo-type dataset
+huggingface-cli upload amazon-qwen-file qwen_meta_Baby_Products_price_sftdata_att_train_180.json --repo-type dataset
+huggingface-cli upload amazon-qwen-file qwen_meta_Baby_Products_Store_sftdata_att_train_180.json --repo-type dataset
+huggingface-cli upload amazon-qwen-file qwen_meta_Baby_Products_title_sftdata_att_train_1800.json --repo-type dataset
+huggingface-cli upload amazon-qwen-file qwen_meta_Video_Games_description_sftdata_att_train_180.json --repo-type dataset
+huggingface-cli upload amazon-qwen-file qwen_meta_Video_Games_details_sftdata_att_train_1800.json --repo-type dataset
+huggingface-cli upload amazon-qwen-file qwen_meta_Video_Games_feature_sftdata_att_train_180.json --repo-type dataset
+huggingface-cli upload amazon-qwen-file qwen_meta_Video_Games_main_category_sftdata_att_train_180.json --repo-type dataset
+huggingface-cli upload amazon-qwen-file qwen_meta_Video_Games_price_sftdata_att_train_30.json --repo-type dataset
+huggingface-cli upload amazon-qwen-file qwen_meta_Video_Games_Store_sftdata_att_train_180.json --repo-type dataset
+huggingface-cli upload amazon-qwen-file qwen_meta_Video_Games_title_sftdata_att_train_1800.json --repo-type dataset
