cfd8eeefd0cd49bac753132b4ade1d567193d802
Noah682 <noahwei682@gmail.com>
2025-05-06 18:53:46 +0800

    Update download.sh

--- download.sh
+++ download.sh
@@ -3,4 +3,4 @@ export HF_TOKEN=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
 huggingface-cli login --token $HF_TOKEN
 
 huggingface-cli download wei682/amazon-grpo --repo-type dataset --local-dir ./msdata/grpo_v1
-# huggingface-cli download wei682/amazon-grpo --repo-type dataset --local-dir ./cpfs01/shared/llm_ddd/zhangyulong/sa_work/msdata/grpo_data
+# huggingface-cli download wei682/amazon-grpo --repo-type dataset --local-dir /cpfs01/shared/llm_ddd/zhangyulong/sa_work/msdata/grpo_data
