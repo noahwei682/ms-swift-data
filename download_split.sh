dd9f0009ac28ed02ba3c8e100739e547f61146d3
Noah682 <noahwei682@gmail.com>
2025-05-07 19:37:30 +0800

    Update download_split.sh

--- download_split.sh
+++ download_split.sh
@@ -1,11 +1,11 @@
-
-
-wei682/split_small
-
-
 pip install huggingface_hub
 export HF_TOKEN=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
 huggingface-cli login --token $HF_TOKEN
 
-huggingface-cli download wei682/amazon-grpo --repo-type dataset --local-dir ./msdata/grpo_v1
+mkdir ./msdata/split_small
+huggingface-cli download wei682/split_small --repo-type dataset --local-dir ./msdata/split_small
 # huggingface-cli download wei682/amazon-grpo --repo-type dataset --local-dir /cpfs01/shared/llm_ddd/zhangyulong/sa_work/msdata/grpo_data
+
+
+mkdir ./wei682/split_small_small
+huggingface-cli download wei682/split_small_small --repo-type dataset --local-dir ./msdata/split_small_small
